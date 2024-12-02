072ba7ef2b4ff6925d044d98828280afa2098119
wwwdiogopt <diogo.gordinho@ua.pt>
2024-12-02 10:34:51 +0000

    Add files via upload
    tarefa 5 cumprida

--- A114859 - Teste 04/js/script.js
+++ A114859 - Teste 04/js/script.js
@@ -1,1 +1,17 @@
 //ficheiro de script
+
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+        var forms = document.getElementsByClassName('needs-validation');
+        Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener('submit', function(event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+})();
